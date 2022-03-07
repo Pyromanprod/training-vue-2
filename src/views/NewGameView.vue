524e48118bdf0c1774cb86c57e90c9a0a093f32b
RenaudKieffer <renaudKieffer.git@gmail.com>
2022-03-07 14:21:15 +0100

    Mise en place d'un squellette rapide

--- src/views/NewGameView.vue
+++ src/views/NewGameView.vue
@@ -2,6 +2,21 @@
   <div class="newgame">
     <h1>Nouvelle partie</h1>
   </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" id="nbTry" placeholder="2">
+          <label for="nbTry">Entrez un nombre entre 1 et 100</label>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="form-floating mb-3">
+          <p>Ici ce sera un recap des nombres essayé</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
