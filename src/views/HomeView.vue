1ab6671955809f9a3f292cc05c654f1959247670
RenaudKieffer <renaudKieffer.git@gmail.com>
2022-03-07 17:02:30 +0100

    Le fonctionnement est là

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
   <div>
     <h1>Test des routing dans vue.JS</h1>
     <p>Bonjour, <br>
-    Ce mini-jeux m'a permis a la fois de travailler les routing dans vue JS, MAis également de renforcer mes acquis en terme d'algorithmie.
+    Ce mini-jeux m'a permis a la fois de travailler les routing dans vue JS, Mais également de renforcer mes acquis en terme d'algorithmie.
     </p>
     <p>
       J'ai également pu continuer a pratiquer le framework
--- src/views/NewGameView.vue
+++ src/views/NewGameView.vue
@@ -1,31 +1,73 @@
 <template>
   <div class="newgame">
-    <h1>Nouvelle partie</h1>
+    <h1 class="mb-5">Nouvelle partie</h1>
   </div>
   <div class="container">
     <div class="row">
       <div class="col-6">
         <div class="form-floating mb-3">
-          <input type="number" class="form-control" id="nbTry" placeholder="2">
+          <input type="number" class="form-control" id="nbTry" placeholder="2" v-model="nbTry"  @keyup.enter="checkNb">
+          <button class="btn btn-secondary mt-3" @click="checkNb">Essayer</button>
           <label for="nbTry">Entrez un nombre entre 1 et 100</label>
         </div>
       </div>
       <div class="col-6">
-        <div class="form-floating mb-3">
           <p>Ici ce sera un recap des nombres essayé</p>
-        </div>
+        <p>Nombre d'essaie {{ nbCoup }}</p>
+        <p>{{ response }}</p>
+        <ul>
+          <li :key='nb.nb' v-for="nb in listNbTry">pour le nombre : {{ nb.nb }} la réponse était : {{ nb.response }}</li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
 
 export default {
   name: 'HomeView',
+  data(){
+    return {
+      response:"",
+      nbCoup:0,
+      secretNb:50,
+      nbTry:'',
+      listNbTry:[]
+    }
+
+  },
+  methods:{
+
+  checkNb(){
+    if (this.nbTry > this.secretNb){
+      this.response = "C'est moins"
+      this.listNbTry.push({nb: this.nbTry, response: "Moins"});
+      this.nbTry = "";
+      this.nbCoup++;
+    }else if(this.nbTry < this.secretNb){
+      this.response = "C'est plus"
+      this.listNbTry.push({nb: this.nbTry, response: "Plus"});
+      this.nbTry = "";
+      this.nbCoup++;
+    }else{
+      this.response = "Bravo Vous avez trouvé en " + this.nbCoup + " Coups"
+    }
+
+  },
+    createNewSecret(){
+
+    },
+  },
   components: {
 
   }
 }
 </script>
+
+<style scoped>
+li{
+  list-style: none;
+}
+</style>
