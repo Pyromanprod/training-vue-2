1e3ea872e545dfbc39414f524567eebc6a98db10
RenaudKieffer <renaudKieffer.git@gmail.com>
2022-03-07 14:08:29 +0100

    Initial comit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+  <div>
+    <h1>Test des routing dans vue.JS</h1>
+    <p>Bonjour, <br>
+    Ce mini-jeux m'a permis a la fois de travailler les routing dans vue JS, MAis également de renforcer mes acquis en terme d'algorithmie.
+    </p>
+    <p>
+      J'ai également pu continuer a pratiquer le framework
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
---
+++ src/views/NewGameView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="newgame">
+    <h1>Nouvelle partie</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
